6b758c3f5d2bf3ed51280da992a2791c2cc0c069
Lam1ro4eQ <alsajgushev@gmail.com>
2025-06-06 13:32:52 +0300

    react hook form

--- todolist/src/features/auth/ui/Login/Login.tsx
+++ todolist/src/features/auth/ui/Login/Login.tsx
@@ -1,6 +1,3 @@
-import { selectThemeMode } from "@/app/app-slice"
-import { useAppSelector } from "@/common/hooks"
-import { getTheme } from "@/common/theme"
 import Button from "@mui/material/Button"
 import Checkbox from "@mui/material/Checkbox"
 import FormControl from "@mui/material/FormControl"
@@ -9,30 +6,33 @@ import FormGroup from "@mui/material/FormGroup"
 import FormLabel from "@mui/material/FormLabel"
 import Grid from "@mui/material/Grid"
 import TextField from "@mui/material/TextField"
-import { useForm } from "react-hook-form"
+import { Controller, SubmitHandler, useForm } from "react-hook-form"
+import styles from "./Login.module.css"
+
+type Inputs = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
 
 export const Login = () => {
-  const themeMode = useAppSelector(selectThemeMode)
+  const {
+    register,
+    reset,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({ defaultValues: { email: "test@test.com", password: "123123123", rememberMe: false } })
 
-  const theme = getTheme(themeMode)
-
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    reset()
+    console.log(data)
+  }
 
   return (
     <Grid container justifyContent={"center"}>
       <FormControl>
         <FormLabel>
-          <p>
-            To login get registered
-            <a
-              style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
-              href="https://social-network.samuraijs.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              here
-            </a>
-          </p>
           <p>or use common test account credentials:</p>
           <p>
             <b>Email:</b> free@samuraijs.com
@@ -41,14 +41,56 @@ export const Login = () => {
             <b>Password:</b> free
           </p>
         </FormLabel>
-        <FormGroup>
-          <TextField label="Email" margin="normal" {...register("example")} />
-          <TextField type="password" label="Password" margin="normal" {...register("example")} />
-          <FormControlLabel label="Remember me" control={<Checkbox />} {...register("example")} />
-          <Button type="submit" variant="contained" color="primary">
-            Login
-          </Button>
-        </FormGroup>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormGroup>
+            <TextField
+              label="Email"
+              margin="normal"
+              error={!!errors.email}
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Incorrect email address",
+                },
+              })}
+            />
+            {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
+            <TextField
+              error={!!errors.password}
+              type="password"
+              label="Password"
+              margin="normal"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "password is required",
+                },
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                  message: "Incorrect password",
+                },
+              })}
+            />
+            {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
+            <FormControlLabel
+              label="Remember me"
+              control={
+                <Controller
+                  name={"rememberMe"}
+                  control={control}
+                  render={({ field: { onChange, value } }) => <Checkbox onChange={(e) => onChange(e.target.checked)} checked={value} />}
+                />
+              }
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
+          </FormGroup>
+        </form>
       </FormControl>
     </Grid>
   )
